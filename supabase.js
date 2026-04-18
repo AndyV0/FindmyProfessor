48db0ae905355c8c432094b3a83d17396a4e0418
AndyV0 <andy06viche@gmail.com>
2026-04-18 05:08:49 -0700

    Update Supabase URL and key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://your-project-url.supabase.co'
-const supabaseKey = 'your-anon-key'
+const supabaseUrl = 'https://pyndqyyyhijbxmkhlllp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5bmRxeXl5aGlqYnhta2hsbGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NzU4MzYsImV4cCI6MjA5MjA1MTgzNn0.GRHQCHRWWKYLHv5R8U_kyEjLxoEkdDxn_ReVV9Kfi7Y'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
